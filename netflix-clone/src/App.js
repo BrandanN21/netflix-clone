4fe65b7cd82f444e449a9f32a2638f017e7d118d
Brandan <blnaef99@gmail.com>
2020-09-09 14:47:15 -0500

    created first component

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1> Hey Brandan Lets build Netflix Clone Front end today</h1>
+      <Row title="Netflix Originals" />
+      <Row title="Trending Now" />
     </div>
   );
 }
