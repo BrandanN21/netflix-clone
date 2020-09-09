167c53a3b59e4c824a45aa630f78fd13b2a4e69a
Brandan <blnaef99@gmail.com>
2020-09-09 15:19:26 -0500

    get async call for data

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 
-function Row({ title }){
+function Row({ title, fetchUrl }){
     // uses a temp memory to hold an array 
     const [movies, setMovies] = useState([]);
 
@@ -11,7 +11,13 @@ function Row({ title }){
     useEffect(() =>{
         // if [], run once when the row loads and don't run it again
         // if != [], run once when loads, and run everytime [] changes
-
+        // need to make an async call 
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            console.log(request)
+            return request;
+        }
+        fetchData();
     }, []);
 
     return (
