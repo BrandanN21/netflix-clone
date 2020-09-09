a1e26054b70453bf4484a036009debeb316761d9
Brandan <blnaef99@gmail.com>
2020-09-09 14:59:57 -0500

    added useEffect statements

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 
 function Row({ title }){
+    // uses a temp memory to hold an array 
+    const [movies, setMovies] = useState([]);
+
+    // a snippet of code which runs based on a specific condition/variable
+    // when row loads run this request for the information from TMBD
+
+    useEffect(() =>{
+        // if [], run once when the row loads and don't run it again
+        // if != [], run once when loads, and run everytime [] changes
+
+    }, []);
+
     return (
         <div>
             <h2>{title}</h2>
-
+            
             {/* container -> posters */}
         </div>
     )
