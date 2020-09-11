6aa94d3d52625c1b687309f47944a273642b00e3
Brandan <blnaef99@gmail.com>
2020-09-11 15:39:37 -0500

    added movie trailers

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   // uses a temp memory to hold an array
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // a snippet of code which runs based on a specific condition/variable
   // when row loads run this request for the information from TMBD
@@ -24,15 +27,36 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]); // *outside variable now becomes a dependency.. REQUIRED*
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
-
       <div className="row_posters">
         {/* serveral row_poster(s) */}
         {movies.map((movie) => (
           <img
             key={movie.id} //optimization, renders each one at a time rather than all at once
+            onClick={() => handleClick(movie)}
             className={`row_poster ${isLargeRow && "row_posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -41,8 +65,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
-
-      {/* container -> posters */}
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
